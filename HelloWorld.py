a4cefac54ecfa24b074fe4e4adec1d1b31fb0a9a
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-03-14 01:10:38 +1100

    first session notes! 13-14/03

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# INTRODUCTION #
+
+# simple, basic syntax.
+print("Hello World!")
+
+# longer syntax, but more specific. let's everyone know what exactly you are printing.
+# declare a variable: hello_string is the variable name
+hello_string = "Hello World!";
+# print that variable
+print(hello_string);
+# allows you to change the value of that variable whenever
+hello_string = "new world";
+print(hello_string);
+
+
