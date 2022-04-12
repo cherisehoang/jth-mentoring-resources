cc5db645bc9c7a2e77ba7007098402fe99364117
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-04-12 23:03:40 +1000

    added lists to variables tutorial

--- python-basics/variables.py
+++ python-basics/variables.py
@@ -19,3 +19,12 @@ true_bool = True
 false_bool = False
 print("This is what a boolean looks like if it returns True: " + true_bool)
 print("This is what a boolean looks like if it returns False: " + false_bool)
+
+# lists - can be called arrays in other languages
+# they all have square brackets! -> []
+number_list = [1,2,3]
+word_list = ["i", "cant", "think", "of", "things", "to", "write"]
+
+# in lists, the counter starts at 0. 
+# take a look at number_list: the first position is position 0, and is the number '1'.
+print(number_list[0])
