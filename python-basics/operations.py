a0cc5cecb45071de7e258a8f5c152c743cbfe7fe
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-04-12 23:15:48 +1000

    added basic arithmetic operations tutorial

---
+++ python-basics/operations.py
@@ -0,0 +1,22 @@
+# Simple Maths Operations
+
+# Addition: +
+num1 = 1 + 2
+
+# Subtraction: -
+num2 = 5 - 3
+
+# Multiplication: *
+num3 = 4 * 7
+
+# Division: /
+num4 = 30 / 6
+
+# Modulus: % (gives you the remainder in a division)
+num5 = 30 % 6 # returns 0
+num6 = 27 % 4 # returns 3
+
+# Exponentials: **
+num7 = 3**2 # returns 3^2 (3 to the power of 2), or 9
+
+print(num1, num2, num3, num4, num5, num6, num7)
