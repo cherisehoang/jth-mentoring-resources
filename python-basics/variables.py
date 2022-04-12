9554b4a0ed9cab445b3e80833b23c0d6bdbcb9e0
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-04-12 22:53:13 +1000

    added a simple variables tutorial

---
+++ python-basics/variables.py
@@ -0,0 +1,21 @@
+# Simple types of variables used in coding, usually the most common ones you'll see
+# You will find new types of variables later on!
+
+# integers - normal, rounded numbers
+integer = 1
+print(integer)
+
+# float - all real numbers, including decimals, exponentials, etc
+# all integers can be floats, but not all floats can be integers
+float_number = 25.034
+print(float_number)
+
+# strings
+some_string = "hello this is a string"
+print(some_string)
+
+# boolean - for all your yes or no questions
+true_bool = True
+false_bool = False
+print("This is what a boolean looks like if it returns True: " + true_bool)
+print("This is what a boolean looks like if it returns False: " + false_bool)
