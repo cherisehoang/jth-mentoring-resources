e3fc3844c2b48466b7b2664c56c33938a3e91fdf
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-04-13 00:03:56 +1000

    added solution for leapChallenge.py

---
+++ python-basics/activities/leapChallenge-solution.py
@@ -0,0 +1,9 @@
+# solution to leapChallenge.py
+
+# assign the years to variables in one line, as per instructions
+year1 = 2004; year2 = 2016
+# all the coding logic in one line
+print(True) if (year1 + year2) % 4 == 0 else print(False)
+
+# you can also assign the years in one line in this way:
+year3, year4 = 2004, 2016
