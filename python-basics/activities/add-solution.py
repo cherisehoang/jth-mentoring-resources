9785b72b9df5698286056194e67b596ab8022705
cherisehoang <63658627+cherisehoang@users.noreply.github.com>
2022-04-12 23:38:38 +1000

    added solution for add.py activity

---
+++ python-basics/activities/add-solution.py
@@ -0,0 +1,15 @@
+# solution for add.py
+
+num1 = 8
+num2 = 10
+sum_number = num1 + num2
+print(sum_number)
+
+
+# faster solution
+num1 = 8
+num2 = 10
+print(num1 + num2)
+
+# an even faster solution
+print(8 + 10)
